6a0a0113e2bf6d8d252ed9458b912f46aa02c3c1
Didier Cherix <didier.cherix@unister.de>
2014-09-12 14:12:00 +0200

    web

---
+++ src/main/java/org/aksw/gerbil/web/RootConfig.java
@@ -0,0 +1,10 @@
+package org.aksw.gerbil.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="org.aksw.batframework.web.config")
+public class RootConfig {
+
+}
