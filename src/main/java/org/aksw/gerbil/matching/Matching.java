536d273b82b8d1ed846b5864b303e0c008b35bd3
m.roeder <michael.roeder@unister.de>
2014-09-22 16:50:47 +0200

    Implemented the Matching creation.

--- src/main/java/org/aksw/gerbil/matching/Matching.java
+++ src/main/java/org/aksw/gerbil/matching/Matching.java
@@ -1,8 +1,25 @@
 package org.aksw.gerbil.matching;
 
+/**
+ * The matching defines how the results of an annotator are compared with the annotations of the dataset.
+ * 
+ * @author m.roeder
+ * 
+ */
 public enum Matching {
-    WEAK_ANNOTATION_MATCH,
+    /**
+     * The matching returns true iff the disambiguated entity of the annotater equals at least one annotation of the
+     * text.
+     */
+    STRONG_ENTITY_MATCH,
+    /**
+     * This matching returns true iff a) the position, b) the length and c) the disambiguated meaning of an entity are
+     * the same as those of the entity in the dataset.
+     */
     STRONG_ANNOTATION_MATCH,
-    CONCEPT_ANNOTATION_MATCH,
-    MENTION_ANNOTATION_MATCH
+    /**
+     * This matching returns true iff the disambiguated meaning of an entity is the same as the meaning of an entity in
+     * the dataset and both entities overlap inside the text.
+     */
+    WEAK_ANNOTATION_MATCH
 }
--- src/main/java/org/aksw/gerbil/matching/MatchingFactory.java
+++ src/main/java/org/aksw/gerbil/matching/MatchingFactory.java
@@ -1,20 +1,40 @@
 package org.aksw.gerbil.matching;
 
 import it.acubelab.batframework.data.Annotation;
+import it.acubelab.batframework.metrics.ConceptAnnotationMatch;
 import it.acubelab.batframework.metrics.MatchRelation;
+import it.acubelab.batframework.metrics.StrongAnnotationMatch;
+import it.acubelab.batframework.metrics.WeakAnnotationMatch;
+import it.acubelab.batframework.utils.WikipediaApiInterface;
+
+import org.aksw.gerbil.datatypes.ExperimentType;
 
 public class MatchingFactory {
 
-    public static MatchRelation<Annotation> createMatchRelation(Matching metric) {
-        switch (metric) {
-        case WEAK_ANNOTATION_MATCH:
-            return null; // TODO
-        case STRONG_ANNOTATION_MATCH:
-            return null; // TODO
-        case CONCEPT_ANNOTATION_MATCH:
-            return null; // TODO
-        case MENTION_ANNOTATION_MATCH:
-            return null; // TODO
+    public static MatchRelation<Annotation> createMatchRelation(WikipediaApiInterface wikiApi, Matching matching,
+            ExperimentType type) {
+        switch (matching) {
+        case WEAK_ANNOTATION_MATCH: {
+            // this is for Sa2W, A2W
+            if (type.equalsOrContainsType(ExperimentType.A2W)) {
+                return new WeakAnnotationMatch(wikiApi);
+            }
+            break;
+        }
+        case STRONG_ANNOTATION_MATCH: {
+            // this is for Sa2W, A2W and D2W
+            if (type.equalsOrContainsType(ExperimentType.D2W)) {
+                return new StrongAnnotationMatch(wikiApi);
+            }
+            break;
+        }
+        case STRONG_ENTITY_MATCH: {
+            // this is for Sc2W, Rc2W and C2W
+            if (ExperimentType.Sc2W.equalsOrContainsType(type)) {
+                return new ConceptAnnotationMatch(wikiApi);
+            }
+            break;
+        }
         }
         return null;
     }
