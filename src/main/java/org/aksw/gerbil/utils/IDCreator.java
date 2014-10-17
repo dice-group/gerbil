f1c8f79f8de47dca11a3a1ac03ab05e42784c520
m.roeder <michael.roeder@unister.de>
2014-10-17 14:16:41 +0200

    Fixed a bug in the IDCreator.

--- src/main/java/org/aksw/gerbil/utils/IDCreator.java
+++ src/main/java/org/aksw/gerbil/utils/IDCreator.java
@@ -10,7 +10,6 @@ public class IDCreator {
     private static final Logger LOGGER = LoggerFactory.getLogger(IDCreator.class);
 
     private static final String ID_FORMAT = "%1$tY%1$tm%1$td%2$04d";
-    private static final long ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
 
     private static IDCreator instance = null;
 
@@ -41,11 +40,11 @@ public class IDCreator {
 
     private synchronized int getCount(Calendar timestamp) {
         // Check whether the given time stamp is still from the same day as the last one
-        if ((timestamp.getTimeInMillis() - lastTimeStamp.getTimeInMillis()) < ONE_DAY_IN_MILLIS) {
+        if ((timestamp.get(Calendar.YEAR) == lastTimeStamp.get(Calendar.YEAR))
+                && (timestamp.get(Calendar.DAY_OF_YEAR) == lastTimeStamp.get(Calendar.DAY_OF_YEAR))) {
             return count++;
         } else {
-            lastTimeStamp.set(Calendar.DAY_OF_MONTH, timestamp.get(Calendar.DAY_OF_MONTH));
-            lastTimeStamp.set(Calendar.MONTH, timestamp.get(Calendar.MONTH));
+            lastTimeStamp.set(Calendar.DAY_OF_YEAR, timestamp.get(Calendar.DAY_OF_YEAR));
             lastTimeStamp.set(Calendar.YEAR, timestamp.get(Calendar.YEAR));
             count = 1;
             return 0;
--- src/test/java/org/aksw/gerbil/utils/IDCreatorTest.java
+++ src/test/java/org/aksw/gerbil/utils/IDCreatorTest.java
@@ -16,4 +16,18 @@ public class IDCreatorTest {
             Assert.assertTrue(id + " doesn't end with " + Integer.toString(i), id.endsWith(Integer.toString(i)));
         }
     }
+
+    @Test
+    public void testIdSet() {
+        IDCreator.getInstance().setLastCreatedID("201410100009");
+        String id = IDCreator.getInstance().createID();
+        // make sure that the count (the last 4 digits) starts at 0
+        Assert.assertEquals("0000", id.substring(id.length() - 4));
+
+        String id2 = IDCreator.getInstance().createID();
+        // make sure that the id is the same except the count
+        Assert.assertEquals("0001", id2.substring(id.length() - 4));
+        Assert.assertEquals(id.substring(0, id.length() - 4), id2.substring(0, id.length() - 4));
+        Assert.assertFalse(id.equals(id2));
+    }
 }
