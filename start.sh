5e4463f3a609871ae73c8f814b1927d7e3f78203
m.roeder <michael.roeder@unister.de>
2014-11-03 12:15:35 +0100

    Created a better solution for the gerbil_keys.properties problem. The start.sh creates an empty file if it doesn't exist. This prevents git from trying to upload the file if it is added to the .gitignore file.

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ mkdir -p "gerbil_data" || exit 1
 
 if [ ! -f "$file" ]; then
     echo "Downloading dependencies ... ($url)"
-    curl --retry 4 -o "$file" "$url"
+    #curl --retry 4 -o "$file" "$url"
 
     if [ ! -f "$file" ]; then
         echo "Couldn't downloading dependency data: $file"
@@ -18,4 +18,18 @@ if [ ! -f "$file" ]; then
     fi
 fi
 
-mvn clean tomcat:run -Dmaven.tomcat.port=1234 &
+echo "Checking properties files..."
+dir="src/main/resources"
+file="$dir/gerbil_keys.properties"
+
+if [ ! -f "$file" ]; then
+	echo "Creating empty $file file"
+	mkdir -p "$dir";
+	echo "##############################################################################"  > $file
+	echo "# This is the properties file contains our keys for several annotator web    #"  >> $file
+	echo "# services.                                                                  #"  >> $file
+	echo "#                      IT SHOULD NOT BE DISTRIBUTED!!!                       #"  >> $file
+	echo "##############################################################################"  >> $file
+fi
+
+#mvn clean tomcat:run -Dmaven.tomcat.port=1234 &
