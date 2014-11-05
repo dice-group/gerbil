4007c50d2bcfe85fc784a385dd8251210c15260f
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-11-05 15:41:15 +0100

    added overview tab and implemented stub for further implementation by @MichaelRoeder refs #12

--- src/main/java/org/aksw/gerbil/web/MainController.java
+++ src/main/java/org/aksw/gerbil/web/MainController.java
@@ -35,155 +35,172 @@ import com.google.common.collect.Sets;
 @Controller
 public class MainController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
-    private static boolean isInitialized = false;
+	private static boolean isInitialized = false;
 
-    private static synchronized void initialize(ExperimentDAO dao) {
-        if (!isInitialized) {
-            String id = dao.getHighestExperimentId();
-            if (id != null) {
-                IDCreator.getInstance().setLastCreatedID(id);
-            }
-            isInitialized = true;
-        }
-        // Simply call the dataset mapping so that it has to be instantiated
-        DatasetMapping.getDatasetsForExperimentType(ExperimentType.Sa2W);
-    }
+	private static synchronized void initialize(ExperimentDAO dao) {
+		if (!isInitialized) {
+			String id = dao.getHighestExperimentId();
+			if (id != null) {
+				IDCreator.getInstance().setLastCreatedID(id);
+			}
+			isInitialized = true;
+		}
+		// Simply call the dataset mapping so that it has to be instantiated
+		DatasetMapping.getDatasetsForExperimentType(ExperimentType.Sa2W);
+	}
 
-    @PostConstruct
-    public void init() {
-        initialize(dao);
-    }
+	@PostConstruct
+	public void init() {
+		initialize(dao);
+	}
 
-    @Autowired
-    @Qualifier("experimentDAO")
-    private ExperimentDAO dao;
+	@Autowired
+	@Qualifier("experimentDAO")
+	private ExperimentDAO dao;
 
-    @RequestMapping("/config")
-    public ModelAndView config() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("config");
-        return model;
-    }
+	@RequestMapping("/config")
+	public ModelAndView config() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("config");
+		return model;
+	}
 
-    @RequestMapping("/about")
-    public ModelAndView about() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("about");
-        return model;
-    }
+	@RequestMapping("/overview")
+	public ModelAndView overview() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("overview");
+		return model;
+	}
+	
+	@RequestMapping("/experimentoverview")
+	public @ResponseBody String experimentoverview(@RequestParam(value = "experimentType") String experimentType, @RequestParam(value = "matching") String matching) {
+		LOGGER.debug("Got request on /experimentoverview");
+//		List<ExperimentTaskResult> results = dao.getMostRecentResults(id);
+//		ExperimentTaskStateHelper.setStatusLines(results);
+	 String data = "["
+	 		+ "		            ['', 'Kia', 'Nissan', 'Toyota', 'Honda'],"
+	 		+ "		            ['2008', 10, 11, 12, 13],"
+	 		+ "		            ['2009', 20, 11, 14, 13],"
+	 		+ "		            ['2010', 30, 15, 12, 13]"
+	 		+ "		          ]";
+//		model.addObject("tasks", results);
+		return data;
+	}
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("about");
+		return model;
+	}
 
-    @RequestMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("index");
-    }
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
 
-    /**
-     * expects a string like
-     * {"type":"A2W","matching":"Mw - weak annotation match","annotator":["A2w one","A2W two"],"dataset":["datasets"]}
-     * 
-     * @param experimentData
-     * @return
-     */
+	/**
+	 * expects a string like
+	 * {"type":"A2W","matching":"Mw - weak annotation match"
+	 * ,"annotator":["A2w one","A2W two"],"dataset":["datasets"]}
+	 * 
+	 * @param experimentData
+	 * @return
+	 */
 
-    @RequestMapping("/execute")
-    public @ResponseBody
-    String execute(@RequestParam(value = "experimentData") String experimentData) {
-        LOGGER.debug("Got request on /execute with experimentData=" + experimentData);
-        Object obj = JSONValue.parse(experimentData);
-        JSONObject configuration = (JSONObject) obj;
-        String type = (String) configuration.get("type");
-        String matching = (String) configuration.get("matching");
-        JSONArray jsonAnnotators = (JSONArray) configuration.get("annotator");
-        String[] annotators = new String[jsonAnnotators.size()];
-        for (int i = 0; i < jsonAnnotators.size(); i++) {
-            annotators[i] = (String) jsonAnnotators.get(i);
-        }
-        JSONArray jsonDataset = (JSONArray) configuration.get("dataset");
-        String[] datasets = new String[jsonDataset.size()];
-        for (int i = 0; i < jsonDataset.size(); i++) {
-            datasets[i] = (String) jsonDataset.get(i);
-        }
-        ExperimentTaskConfiguration[] configs = new ExperimentTaskConfiguration[annotators.length * datasets.length];
-        int count = 0;
-        for (String annotator : annotators) {
-            for (String dataset : datasets) {
-                configs[count] = new ExperimentTaskConfiguration(AnnotatorMapping.getAnnotatorConfig(annotator),
-                        DatasetMapping.getDatasetConfig(dataset), ExperimentType.valueOf(type),
-                        getMatching(matching));
-                LOGGER.debug("Created config: " + configs[count]);
-                ++count;
-            }
-        }
-        String experimentId = IDCreator.getInstance().createID();
-        Experimenter exp = new Experimenter(SingletonWikipediaApi.getInstance(), dao, configs, experimentId);
-        exp.run();
+	@RequestMapping("/execute")
+	public @ResponseBody
+	String execute(@RequestParam(value = "experimentData") String experimentData) {
+		LOGGER.debug("Got request on /execute with experimentData=" + experimentData);
+		Object obj = JSONValue.parse(experimentData);
+		JSONObject configuration = (JSONObject) obj;
+		String type = (String) configuration.get("type");
+		String matching = (String) configuration.get("matching");
+		JSONArray jsonAnnotators = (JSONArray) configuration.get("annotator");
+		String[] annotators = new String[jsonAnnotators.size()];
+		for (int i = 0; i < jsonAnnotators.size(); i++) {
+			annotators[i] = (String) jsonAnnotators.get(i);
+		}
+		JSONArray jsonDataset = (JSONArray) configuration.get("dataset");
+		String[] datasets = new String[jsonDataset.size()];
+		for (int i = 0; i < jsonDataset.size(); i++) {
+			datasets[i] = (String) jsonDataset.get(i);
+		}
+		ExperimentTaskConfiguration[] configs = new ExperimentTaskConfiguration[annotators.length * datasets.length];
+		int count = 0;
+		for (String annotator : annotators) {
+			for (String dataset : datasets) {
+				configs[count] = new ExperimentTaskConfiguration(AnnotatorMapping.getAnnotatorConfig(annotator), DatasetMapping.getDatasetConfig(dataset), ExperimentType.valueOf(type), getMatching(matching));
+				LOGGER.debug("Created config: " + configs[count]);
+				++count;
+			}
+		}
+		String experimentId = IDCreator.getInstance().createID();
+		Experimenter exp = new Experimenter(SingletonWikipediaApi.getInstance(), dao, configs, experimentId);
+		exp.run();
 
-        return experimentId;
-    }
+		return experimentId;
+	}
 
-    private Matching getMatching(String matching) {
-        String matchingName = matching.substring(matching.indexOf('-') + 1).trim().toUpperCase().replace(' ', '_');
-        Matching m = Matching.valueOf(matchingName);
-        return m;
-    }
+	private Matching getMatching(String matching) {
+		String matchingName = matching.substring(matching.indexOf('-') + 1).trim().toUpperCase().replace(' ', '_');
+		Matching m = Matching.valueOf(matchingName);
+		return m;
+	}
 
-    @RequestMapping("/experiment")
-    public ModelAndView experiment(@RequestParam(value = "id") String id) {
-        LOGGER.debug("Got request on /experiment with id=" + id);
-        List<ExperimentTaskResult> results = dao.getResultsOfExperiment(id);
-        ExperimentTaskStateHelper.setStatusLines(results);
-        ModelAndView model = new ModelAndView();
-        model.setViewName("experiment");
-        model.addObject("tasks", results);
-        return model;
-    }
+	@RequestMapping("/experiment")
+	public ModelAndView experiment(@RequestParam(value = "id") String id) {
+		LOGGER.debug("Got request on /experiment with id=" + id);
+		List<ExperimentTaskResult> results = dao.getResultsOfExperiment(id);
+		ExperimentTaskStateHelper.setStatusLines(results);
+		ModelAndView model = new ModelAndView();
+		model.setViewName("experiment");
+		model.addObject("tasks", results);
+		return model;
+	}
 
-    @RequestMapping("/exptypes")
-    public @ResponseBody
-    List<ExperimentType> expTypes() {
-        return Arrays.asList(ExperimentType.values());
-    }
+	@RequestMapping("/exptypes")
+	public @ResponseBody
+	List<ExperimentType> expTypes() {
+		return Arrays.asList(ExperimentType.values());
+	}
 
-    @RequestMapping("/matchings")
-    public @ResponseBody
-    Set<String> matchingsForExpType(@RequestParam(value = "experimentType") String experimentType) {
-        ExperimentType type = ExperimentType.valueOf(experimentType);
-        switch (type) {
-        case C2W:
-            return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
-        case D2W:
-            // Mw will not be shown since the positions are always exact and thus it works like Ma
-            return Sets.newLinkedHashSet(Lists.newArrayList("Ma - strong annotation match"));
-        case A2W:
-            return Sets.newLinkedHashSet(Lists.newArrayList("Mw - weak annotation match",
-                    "Ma - strong annotation match"));
-        case Rc2W:
-            return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
-        case Sc2W:
-            return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
-        case Sa2W:
-            return Sets.newLinkedHashSet(Lists.newArrayList("Mw - weak annotation match",
-                    "Ma - strong annotation match"));
-        default:
-            return Sets.newLinkedHashSet(Lists.newArrayList("None"));
-        }
-    }
+	@RequestMapping("/matchings")
+	public @ResponseBody
+	Set<String> matchingsForExpType(@RequestParam(value = "experimentType") String experimentType) {
+		ExperimentType type = ExperimentType.valueOf(experimentType);
+		switch (type) {
+		case C2W:
+			return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
+		case D2W:
+			// Mw will not be shown since the positions are always exact and
+			// thus it works like Ma
+			return Sets.newLinkedHashSet(Lists.newArrayList("Ma - strong annotation match"));
+		case A2W:
+			return Sets.newLinkedHashSet(Lists.newArrayList("Mw - weak annotation match", "Ma - strong annotation match"));
+		case Rc2W:
+			return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
+		case Sc2W:
+			return Sets.newLinkedHashSet(Lists.newArrayList("Me - strong entity match"));
+		case Sa2W:
+			return Sets.newLinkedHashSet(Lists.newArrayList("Mw - weak annotation match", "Ma - strong annotation match"));
+		default:
+			return Sets.newLinkedHashSet(Lists.newArrayList("None"));
+		}
+	}
 
-    @RequestMapping("/annotators")
-    public @ResponseBody
-    Set<String> annotatorsForExpType(@RequestParam(value = "experimentType") String experimentType) {
-        return AnnotatorMapping.getAnnotatorsForExperimentType(ExperimentType
-                .valueOf(experimentType));
-    }
+	@RequestMapping("/annotators")
+	public @ResponseBody
+	Set<String> annotatorsForExpType(@RequestParam(value = "experimentType") String experimentType) {
+		return AnnotatorMapping.getAnnotatorsForExperimentType(ExperimentType.valueOf(experimentType));
+	}
 
-    @RequestMapping("/datasets")
-    public @ResponseBody
-    Set<String> datasets(@RequestParam(value = "experimentType") String experimentType) {
-        Set<String> datasets = DatasetMapping.getDatasetsForExperimentType(ExperimentType
-                .valueOf(experimentType));
-        return datasets;
-    }
+	@RequestMapping("/datasets")
+	public @ResponseBody
+	Set<String> datasets(@RequestParam(value = "experimentType") String experimentType) {
+		Set<String> datasets = DatasetMapping.getDatasetsForExperimentType(ExperimentType.valueOf(experimentType));
+		return datasets;
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/views/overview.jsp
@@ -0,0 +1,101 @@
+<%@page import="org.aksw.gerbil.web.ExperimentTaskStateHelper"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<head>
+<link rel="stylesheet" href="webjars/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet"href="webjars/bootstrap-multiselect/0.9.8/css/bootstrap-multiselect.css" />
+<style type="text/css">
+/* making the buttons wide enough and right-aligned */
+.btn-group>.btn {
+	float: none;
+	width: 100%;
+	text-align: right;
+}
+
+.btn-group {
+	width: 100%;
+}
+
+#type {
+	text-align: right;
+}
+
+#type>option {
+	text-align: right;
+}
+</style>
+
+</head>
+<body class="container">
+	<!-- mappings to URLs in back-end controller -->
+	<c:url var="experiment" value="/experimentoverview" />
+
+	<script src="webjars/jquery/2.1.1/jquery.min.js"></script>
+	<script src="webjars/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="webjars/tablesorter/2.15.5/js/jquery.tablesorter.js"></script>
+
+	<%@include file="navbar.jsp"%>
+	<h1>GERBIL Experiment Overview</h1>
+	<c:if test="${not empty tasks}">
+	Type: <c:out value="${tasks[0].type}" />
+		</br>
+	Matching: <c:out value="${tasks[0].matching}" />
+		<table id="resultTable"
+			class="table  table-hover table-condensed tablesorter tableScroll">
+			<thead>
+				<tr>
+					<th>Annotator</th>
+					<th>Dataset</th>
+					<th>Micro F1</th>
+					<th>Micro Precision</th>
+					<th>Micro Recall</th>
+					<th>Macro F1</th>
+					<th>Macro Precision</th>
+					<th>Macro Recall</th>
+					<!-- <th>State</th> -->
+					<th>Error Count</th>
+					<th>Timestamp</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="task" items="${tasks}">
+					<tr>
+						<td>${task.annotator}</td>
+						<td>${task.dataset}</td>
+						<c:if test="${empty task.stateMsg}">
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.microF1Measure}" /></td>
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.microPrecision}" /></td>
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.microRecall}" /></td>
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.macroF1Measure}" /></td>
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.macroPrecision}" /></td>
+							<td><fmt:formatNumber type="number" maxFractionDigits="4"
+									value="${task.macroRecall}" /></td>
+							<!-- <td>${task.state}</td> -->
+							<td>${task.errorCount}</td>
+						</c:if>
+						<c:if test="${not empty task.stateMsg}">
+							<td colspan="7" style="text-align:center">${task.stateMsg}</td>
+						</c:if>
+						<td>${task.timestampstring}</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</c:if>
+
+
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+	        $("#resultTable").tablesorter({
+		        sortList : [ [ 0, 0 ], [ 1, 0 ] ]
+	        });
+        });
+	</script>
+</body>
