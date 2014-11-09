4b5a7b5df117fe7d35d763f42f9a61154749337a
MichaelRoeder <michaderstudent@hotmail.de>
2014-11-09 16:51:45 +0100

    Added mapping methods to the MainController class returning the GERBIL vocabulary definition depending on the Accept header of the request. (refs issue #6)

--- src/main/java/org/aksw/gerbil/web/MainController.java
+++ src/main/java/org/aksw/gerbil/web/MainController.java
@@ -1,5 +1,7 @@
 package org.aksw.gerbil.web;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +22,7 @@ import org.aksw.gerbil.utils.AnnotatorMapping;
 import org.aksw.gerbil.utils.DatasetMapping;
 import org.aksw.gerbil.utils.IDCreator;
 import org.aksw.gerbil.utils.SingletonWikipediaApi;
+import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
@@ -40,6 +43,10 @@ import com.google.common.collect.Sets;
 public class MainController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
+    private static final String GERBIL_VOCABULARY_JSON_FILE = "vocab/gerbil.json";
+    private static final String GERBIL_VOCABULARY_RDF_FILE = "vocab/gerbil.rdf";
+    private static final String GERBIL_VOCABULARY_TTL_FILE = "vocab/gerbil.ttl";
 
     private static boolean isInitialized = false;
 
@@ -274,5 +281,37 @@ public class MainController {
             resultBuilder.append("</p>\n");
         }
         return resultBuilder.toString();
+    }
+
+    @RequestMapping(value = "/vocab*", produces = { "application/json+ld", "application/json" })
+    public @ResponseBody
+    String vocabularyAsJSON() {
+        return getResourceAsString(GERBIL_VOCABULARY_JSON_FILE);
+    }
+
+    @RequestMapping(value = "/vocab*", produces = "application/rdf+xml")
+    public @ResponseBody
+    String vocabularyAsRDFXML() {
+        return getResourceAsString(GERBIL_VOCABULARY_RDF_FILE);
+    }
+
+    @RequestMapping(value = "/vocab*", produces = { "text/turtle", "text/plain" })
+    public @ResponseBody
+    String vocabularyAsTTL() {
+        return getResourceAsString(GERBIL_VOCABULARY_TTL_FILE);
+    }
+
+    private String getResourceAsString(String resource) {
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resource);
+        if (is != null) {
+            try {
+                return IOUtils.toString(is, "utf-8");
+            } catch (IOException e) {
+                LOGGER.error("Exception while loading vocabulary resource. Returning null.", e);
+            } finally {
+                IOUtils.closeQuietly(is);
+            }
+        }
+        return null;
     }
 }
