af862ae7f33df228d95e120e61e35ac6aedacc61
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-11-17 13:22:42 +0100

    script for changing old data in database to new java code

---
+++ src/main/resources/spring/database/changeNames.sql
@@ -0,0 +1,8 @@
+UPDATE ExperimentTasks SET experimentType='D2KB' WHERE experimentType='D2W';
+UPDATE ExperimentTasks SET experimentType='A2KB' WHERE experimentType='A2W';
+UPDATE ExperimentTasks SET experimentType='Sa2KB' WHERE experimentType='Sa2W';
+UPDATE ExperimentTasks SET experimentType='C2KB' WHERE experimentType='C2W';
+UPDATE ExperimentTasks SET experimentType='Sc2KB' WHERE experimentType='Sc2W';
+UPDATE ExperimentTasks SET experimentType='Rc2KB' WHERE experimentType='Rc2W';
+
+UPDATE ExperimentTasks SET annotatorName='Babelfy' WHERE annotatorName='BabelFy';
