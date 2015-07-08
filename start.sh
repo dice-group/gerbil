0e2b01b2095b60a02286831f95ed91a2b96886c9
MichaelRoeder <michaderstudent@hotmail.de>
2015-07-08 12:43:48 +0200

    Added the OKE2015 address of the gerbil_data.zip file to the start.sh script.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 echo "Checking dependencies..."
 file="gerbil_data/gerbil_data.zip"
-url="http://github.com/AKSW/gerbil/releases/download/v1.0.0/gerbil_data.zip"
+url="http://github.com/AKSW/gerbil/releases/download/OKE2015/gerbil_data.zip"
 
 mkdir -p "gerbil_data" || exit 1
 
