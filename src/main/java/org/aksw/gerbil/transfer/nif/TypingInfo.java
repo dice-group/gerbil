1fc3a8e8f9ce08c2b117a4ef4dfa8dde75d8f5bc
MichaelRoeder <michaderstudent@hotmail.de>
2015-09-30 14:48:57 +0200

    Updated the licenses inside the single files.

--- src/main/java/org/aksw/gerbil/transfer/nif/TypingInfo.java
+++ src/main/java/org/aksw/gerbil/transfer/nif/TypingInfo.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of NIF transfer library for the General Entity Annotator Benchmark.
+ *
+ * NIF transfer library for the General Entity Annotator Benchmark is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NIF transfer library for the General Entity Annotator Benchmark is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NIF transfer library for the General Entity Annotator Benchmark.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.aksw.gerbil.transfer.nif;
 
 import java.util.Collection;
