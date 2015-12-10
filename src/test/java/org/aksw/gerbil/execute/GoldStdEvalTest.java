17b052698d56af8c96ee1347077d7ce9d5864e7e
MichaelRoeder <michaderstudent@hotmail.de>
2015-12-10 12:56:48 +0100

    Extended the gold standard based evaluation test to all non-deprecated experiment types and the datasets known for them.

--- src/test/java/org/aksw/gerbil/execute/GoldStdEvalTest.java
+++ src/test/java/org/aksw/gerbil/execute/GoldStdEvalTest.java
@@ -20,7 +20,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.aksw.gerbil.annotator.AnnotatorConfiguration;
+import org.aksw.gerbil.annotator.TestA2KBAnnotator;
+import org.aksw.gerbil.annotator.TestC2KBAnnotator;
 import org.aksw.gerbil.annotator.TestD2KBAnnotator;
+import org.aksw.gerbil.annotator.TestEntityRecognizer;
+import org.aksw.gerbil.annotator.TestEntityTyper;
+import org.aksw.gerbil.annotator.TestOKETask1Annotator;
+import org.aksw.gerbil.annotator.TestOKETask2Annotator;
 import org.aksw.gerbil.database.SimpleLoggingResultStoringDAO4Debugging;
 import org.aksw.gerbil.dataset.Dataset;
 import org.aksw.gerbil.dataset.DatasetConfiguration;
@@ -33,6 +40,7 @@ import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.matching.Matching;
 import org.aksw.gerbil.semantic.sameas.SameAsRetriever;
 import org.aksw.gerbil.semantic.sameas.impl.ErrorFixingSameAsRetriever;
+import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.web.config.AdapterList;
 import org.aksw.gerbil.web.config.DatasetsConfig;
 import org.aksw.gerbil.web.config.RootConfig;
@@ -50,9 +58,8 @@ import org.junit.runners.Parameterized.Parameters;
  * 
  */
 @RunWith(Parameterized.class)
-public class D2KBGoldTest extends AbstractExperimentTaskTest {
+public class GoldStdEvalTest extends AbstractExperimentTaskTest {
 
-    private static final ExperimentType EXPERIMENT_TYPE = ExperimentType.D2KB;
     private static final EvaluatorFactory EVALUATOR_FACTORY = RootConfig
             .createEvaluatorFactory(RootConfig.createSubClassInferencer());
     private static final SameAsRetriever SAME_AS_RETRIEVER = new ErrorFixingSameAsRetriever();
@@ -60,30 +67,38 @@ public class D2KBGoldTest extends AbstractExperimentTaskTest {
     private static final Matching MATCHING = Matching.STRONG_ENTITY_MATCH;
 
     @Parameters
-    public static Collection<Object[]> data() {
+    public static Collection<Object[]> data() throws NoSuchFieldException, SecurityException {
         List<Object[]> testConfigs = new ArrayList<Object[]>();
         AdapterList<DatasetConfiguration> adapterList = DatasetsConfig.datasets(ENTITY_CHECKER_MANAGER,
                 SAME_AS_RETRIEVER);
-        List<DatasetConfiguration> datasetConfigs = adapterList.getAdaptersForExperiment(EXPERIMENT_TYPE);
-//        for (DatasetConfiguration datasetConfig : datasetConfigs) {
-//            testConfigs.add(new Object[] { datasetConfig });
-//        }
-        testConfigs.add(new Object[] { datasetConfigs.get(2) });
+        List<DatasetConfiguration> datasetConfigs = null;
+        for (ExperimentType experimentType : ExperimentType.values()) {
+            if (ExperimentType.class.getDeclaredField(experimentType.name()).getAnnotation(Deprecated.class) == null) {
+                datasetConfigs = adapterList.getAdaptersForExperiment(experimentType);
+                for (DatasetConfiguration datasetConfig : datasetConfigs) {
+                    testConfigs.add(new Object[] { experimentType, datasetConfig });
+                }
+            }
+        }
         return testConfigs;
     }
 
-    public static void main(String[] args) throws GerbilException {
-        AdapterList<DatasetConfiguration> adapterList = DatasetsConfig.datasets(ENTITY_CHECKER_MANAGER,
-                SAME_AS_RETRIEVER);
-        List<DatasetConfiguration> datasetConfigs = adapterList.getAdaptersForExperiment(EXPERIMENT_TYPE);
-        for (DatasetConfiguration datasetConfig : datasetConfigs) {
-            (new D2KBGoldTest(datasetConfig)).test();
-        }
-    }
+    // public static void main(String[] args) throws GerbilException {
+    // AdapterList<DatasetConfiguration> adapterList =
+    // DatasetsConfig.datasets(ENTITY_CHECKER_MANAGER,
+    // SAME_AS_RETRIEVER);
+    // List<DatasetConfiguration> datasetConfigs =
+    // adapterList.getAdaptersForExperiment(EXPERIMENT_TYPE);
+    // for (DatasetConfiguration datasetConfig : datasetConfigs) {
+    // (new GoldStdEvalTest(datasetConfig)).test();
+    // }
+    // }
 
+    private ExperimentType experimentType;
     private DatasetConfiguration datasetConfig;
 
-    public D2KBGoldTest(DatasetConfiguration datasetConfig) {
+    public GoldStdEvalTest(ExperimentType experimentType, DatasetConfiguration datasetConfig) {
+        this.experimentType = experimentType;
         this.datasetConfig = datasetConfig;
     }
 
@@ -92,49 +107,39 @@ public class D2KBGoldTest extends AbstractExperimentTaskTest {
         int experimentTaskId = 1;
         SimpleLoggingResultStoringDAO4Debugging experimentDAO = new SimpleLoggingResultStoringDAO4Debugging();
 
-        Dataset dataset = datasetConfig.getDataset(EXPERIMENT_TYPE);
+        Dataset dataset = datasetConfig.getDataset(experimentType);
         Assert.assertNotNull(dataset);
 
-        // ExperimentTaskConfiguration configuration = new
-        // ExperimentTaskConfiguration(
-        // new TestD2KBAnnotator(copy(dataset.getInstances())), datasetConfig,
-        // EXPERIMENT_TYPE, MATCHING);
         ExperimentTaskConfiguration configuration = new ExperimentTaskConfiguration(
-                new TestD2KBAnnotator(dataset.getInstances()), datasetConfig, EXPERIMENT_TYPE, MATCHING);
-        runTest(experimentTaskId, experimentDAO, SAME_AS_RETRIEVER, EVALUATOR_FACTORY, configuration, new F1MeasureTestingObserver(this,
-                experimentTaskId, experimentDAO, new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0 }));
+                createTestAnnotator(experimentType, dataset.getInstances()), datasetConfig, experimentType, MATCHING);
+        runTest(experimentTaskId, experimentDAO, SAME_AS_RETRIEVER, EVALUATOR_FACTORY, configuration,
+                new F1MeasureTestingObserver(this, experimentTaskId, experimentDAO,
+                        new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0 }));
     }
 
-    // private List<Document> copy(List<Document> instances) {
-    // List<Document> copies = new ArrayList<Document>(instances.size());
-    // for (Document document : instances) {
-    // copies.add(copy(document));
-    // }
-    // return copies;
-    // }
-    //
-    // private Document copy(Document document) {
-    // return new DocumentImpl(document.getText(), document.getDocumentURI(),
-    // copyMarkings(document.getMarkings()));
-    // }
-    //
-    // private List<Marking> copyMarkings(List<Marking> markings) {
-    // List<Marking> copies = new ArrayList<Marking>(markings.size());
-    // for (Marking marking : markings) {
-    // copies.add(copy(marking));
-    // }
-    // return copies;
-    // }
-    //
-    // private Marking copy(Marking marking) {
-    // if (marking instanceof ScoredTypedNamedEntity) {
-    // return new ScoredTypedNamedEntity(startPosition, length, uris, types,
-    // confidence);
-    // }
-    // return null;
-    // }
-    //
-    // private Set<String> copyUris(Set<String> uris) {
-    // Set<String> copy = new HashSet()<String>();
-    // }
+    @SuppressWarnings("deprecation")
+    private AnnotatorConfiguration createTestAnnotator(ExperimentType experimentType, List<Document> instances) {
+        switch (experimentType) {
+        case A2KB:
+            return new TestA2KBAnnotator(instances);
+        case C2KB:
+            return new TestC2KBAnnotator(instances);
+        case D2KB:
+            return new TestD2KBAnnotator(instances);
+        case ERec:
+            return new TestEntityRecognizer(instances);
+        case ETyping:
+            return new TestEntityTyper(instances);
+        case OKE_Task1:
+            return new TestOKETask1Annotator(instances);
+        case OKE_Task2:
+            return new TestOKETask2Annotator(instances);
+        case Rc2KB:
+        case Sa2KB:
+        case Sc2KB:
+        default:
+        }
+        return null;
+    }
+
 }
