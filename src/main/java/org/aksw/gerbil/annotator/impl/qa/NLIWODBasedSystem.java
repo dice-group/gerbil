35cee8d71c5deea2d97a871c5285d965462ad3d1
TortugaAttack <felixconrads@googlemail.com>
2016-06-30 12:37:10 +0200

    fixed #136

--- src/main/java/org/aksw/gerbil/annotator/impl/qa/NLIWODBasedSystem.java
+++ src/main/java/org/aksw/gerbil/annotator/impl/qa/NLIWODBasedSystem.java
@@ -18,6 +18,11 @@ import org.aksw.qa.systems.SINA;
 import org.aksw.qa.systems.START;
 import org.aksw.qa.systems.YODA;
 
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.sparql.core.Prologue;
+
 public class NLIWODBasedSystem extends AbstractAnnotator implements QASystem {
 
     public static final String HAWK_SYSTEM_NAME = "HAWK";
@@ -25,9 +30,10 @@ public class NLIWODBasedSystem extends AbstractAnnotator implements QASystem {
     public static final String SINA_SYSTEM_NAME = "SINA";
     public static final String START_SYSTEM_NAME = "START";
     public static final String YODA_SYSTEM_NAME = "YODA";
-
+    
     protected ASystem qaSystem;
-
+	
+     
     public NLIWODBasedSystem(String systemName) throws GerbilException {
         switch (systemName) {
         case HAWK_SYSTEM_NAME: {
@@ -53,12 +59,15 @@ public class NLIWODBasedSystem extends AbstractAnnotator implements QASystem {
         default:
             throw new GerbilException("Got an unknown system name \"" + systemName + "\".",
                     ErrorTypes.ANNOTATOR_LOADING_ERROR);
-        }
+		}
+    	
     }
 
     @Override
     public List<Marking> answerQuestion(Document document) throws GerbilException {
         IQuestion question = qaSystem.search(document.getText());
+        if(question.getSparqlQuery()!=null)
+        	question.setSparqlQuery(replacePrefixes(question.getSparqlQuery(), PrefixMapping.Extended));
         Document resultDoc = QAUtils.translateQuestion(question, document.getDocumentURI());
         if(resultDoc != null) {
             return resultDoc.getMarkings();
@@ -67,4 +76,19 @@ public class NLIWODBasedSystem extends AbstractAnnotator implements QASystem {
         }
     }
 
+    //TODO find a better class for this method
+    public static String replacePrefixes(String query, PrefixMapping pmap){
+        /* With Prologue and the parse method, 
+         * the queryString gets parsed without an error
+         */
+        Prologue prog = new Prologue();
+        prog.setPrefixMapping(pmap);
+        Query q = QueryFactory.parse(new Query(prog), query, null, null);
+        //Set Prefix Mapping
+        q.setPrefixMapping(pmap);
+        //remove PrefixMapping so the prefixes will get replaced by the full uris
+        q.setPrefixMapping(null);       
+        return q.serialize();
+    }
+    
 }
