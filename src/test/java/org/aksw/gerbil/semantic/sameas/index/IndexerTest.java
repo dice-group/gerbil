4b9ff33efe473c13e5f0c282567b06bec4728895
TortugaAttack <felixconrads@googlemail.com>
2016-09-02 15:24:02 +0200

    indexing unit test

--- src/test/java/org/aksw/gerbil/semantic/sameas/index/IndexerTest.java
+++ src/test/java/org/aksw/gerbil/semantic/sameas/index/IndexerTest.java
@@ -8,7 +8,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.aksw.gerbil.exceptions.GerbilException;
-import org.aksw.gerbil.semantic.sameas.index.LuceneConstants.IndexingStrategy;
 import org.junit.Test;
 
 public class IndexerTest {
@@ -19,12 +18,12 @@ public class IndexerTest {
 		//Test if indexing and searching works
 		//1. make some same as retrievals
 		//2. index them
-		Indexer index = new Indexer("test", IndexingStrategy.TermQuery);
+		Indexer index = new Indexer("test");
 		index.index("http://dbpedia.org/a", getList("http://dbpedia.org"));
 		index.index("http://wikipedia.org/a", getList("http://wikipedia.org"));
 		index.index("http://de.dbpedia.org/a", getList("http://de.dbpedia.org"));
 		//3. search for one that exists
-		Searcher search = new Searcher("test", IndexingStrategy.TermQuery);
+		Searcher search = new Searcher("test");
 		assertFalse(search.search("http://wikipedia.org/a").isEmpty());
 		assertFalse(search.search("http://wikipedia.org/d").isEmpty());
 		assertFalse(search.search("http://dbpedia.org/a").isEmpty());
@@ -38,12 +37,12 @@ public class IndexerTest {
 		//Test if indexing and searching works
 		//1. make some same as retrievals
 		//2. index them
-		Indexer index = new Indexer("test", IndexingStrategy.WildCard);
-		index.indexSameAs(getList("http://dbpedia.org"));
-		index.indexSameAs(getList("http://wikipedia.org"));
-		index.indexSameAs(getList("http://de.dbpedia.org"));
+		Indexer index = new Indexer("test");
+		index.indexSameAs("http://dbpedia.org/a", getList("http://dbpedia.org"));
+		index.indexSameAs("http://wikipedia.org/a", getList("http://wikipedia.org"));
+		index.indexSameAs("http://de.dbpedia.org/a", getList("http://de.dbpedia.org"));
 		//3. search for one that exists
-		Searcher search = new Searcher("test", IndexingStrategy.WildCard);
+		Searcher search = new Searcher("test");
 		assertFalse(search.search("http://wikipedia.org/a").isEmpty());
 		assertFalse(search.search("http://dbpedia.org/a").isEmpty());
 		//4. search for one that dont exist		
