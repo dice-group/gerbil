abb0c42091a863a58f226018f56719d06e5e0011
MichaelRoeder <michaasresearcher@gmail.com>
2016-11-16 17:27:36 +0100

    Added the downloading of the DBpedia entity check index to the start.sh script. Set Micoposts2013 pack to Entity Typing. Added the german and french dbpedia resources to the entity check index.

--- start.sh
+++ start.sh
@@ -151,7 +151,7 @@ fi
 echo "Checking dbpedia sameAs index..."
 if [ ! -d "indexes/dbpedia" ]; then
 	echo "Couldn't find a dbpedia sameAs index"
-	if yesno "Should the index be downloaded (~1GB)? (yes/no): "; then
+	if yesno "Should the index be downloaded (~1GB zipped, ~2GB extracted)? (yes/no): "; then
 		mkdir -p "indexes/dbpedia" || error "Could not create indexes/dbpedia directory"
 		file="indexes/dbpedia/dbpedia_index.zip"
 		url="http://139.18.2.164/mroeder/gerbil/dbpedia_index.zip"
@@ -163,6 +163,27 @@ if [ ! -d "indexes/dbpedia" ]; then
 		else
 			echo "Extracting index ... "
 			unzip "$file" -d "indexes/dbpedia"
+		fi
+	fi
+fi
+
+#####################################################################
+# Check for dbpedia entity check index
+echo "Checking dbpedia entity check index..."
+if [ ! -d "indexes/dbpedia_check" ]; then
+	echo "Couldn't find a dbpedia entity check index"
+	if yesno "Should the index be downloaded (~0.3GB zipped, ~0.7GB extracted)? (yes/no): "; then
+		mkdir -p "indexes/dbpedia_check" || error "Could not create indexes/dbpedia_check directory"
+		file="indexes/dbpedia_check/dbpedia_check_index.zip"
+		url="http://139.18.2.164/mroeder/gerbil/dbpedia_check_index.zip"
+		echo "Downloading index ... ($url)"
+		curl --retry 4 -L -o "$file" "$url"
+
+		if [ ! -f "$file" ]; then
+			echo "Couldn't downloading index file: $file"
+		else
+			echo "Extracting index ... "
+			unzip "$file" -d "indexes/dbpedia_check"
 		fi
 	fi
 fi
