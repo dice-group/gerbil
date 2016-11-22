6a3d59d1144da515f6ce335b2ce68b49f10609d3
TortugaAttack <felixconrads@googlemail.com>
2016-11-22 22:56:25 +0100

    #46 WSDM Dataset added, senseval properties fixed, minor spelling error fixed

--- src/main/java/org/aksw/gerbil/dataset/impl/wsdm/WSDMDataset.java
+++ src/main/java/org/aksw/gerbil/dataset/impl/wsdm/WSDMDataset.java
@@ -1,20 +1,4 @@
-/**
- * This file is part of General Entity Annotator Benchmark.
- *
- * General Entity Annotator Benchmark is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * General Entity Annotator Benchmark is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with General Entity Annotator Benchmark.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.aksw.gerbil.dataset.impl.micro;
+package org.aksw.gerbil.dataset.impl.wsdm;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,30 +15,30 @@ import org.aksw.gerbil.datatypes.ErrorTypes;
 import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.Marking;
+import org.aksw.gerbil.transfer.nif.data.Annotation;
 import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
-import org.aksw.gerbil.transfer.nif.data.TypedNamedEntity;
+import org.aksw.gerbil.utils.WikipediaHelper;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author Giuseppe Rizzo (giuse.rizzo@gmail.com)
- * @author Michael R&ouml;der (roeder@informatik.uni-leipzig.de)
- */
-public class Microposts2016Dataset extends AbstractDataset implements
-		InitializableDataset {
+public class WSDMDataset extends AbstractDataset implements
+						InitializableDataset {
 
 	private static final Logger LOGGER = LoggerFactory
-			.getLogger(Microposts2016Dataset.class);
-
+			.getLogger(WSDMDataset.class);
+	
+	private static String WIKIPEDIA_DOMAIN = "en.wikipedia.org";
 	protected List<Document> documents;
 	private String annotatedFile;
 	private String tweetsFile;
 
-	public Microposts2016Dataset(String annotatedFile, String tweetsFile) {
+	public WSDMDataset(String annotatedFile, String tweetsFile){
 		this.annotatedFile = annotatedFile;
 		this.tweetsFile = tweetsFile;
 	}
-
+	
+	
 	@Override
 	public int size() {
 		return documents.size();
@@ -67,27 +51,23 @@ public class Microposts2016Dataset extends AbstractDataset implements
 
 	@Override
 	public void init() throws GerbilException {
-		this.documents = loadDocuments(new File(annotatedFile), new File(
-				tweetsFile));
+		this.documents = loadDocuments(new File(annotatedFile), new File(tweetsFile));
 	}
 
-	protected List<Document> loadDocuments(File annotations, File tweetsFile)
+	private List<Document> loadDocuments(File annotations, File tweets)
 			throws GerbilException {
-
 		List<Document> documents = new ArrayList<Document>();
 		String documentUriPrefix = "http://" + getName() + "/";
-
+		//its json per line 
 		try (BufferedReader bReader = new BufferedReader(new FileReader(
 				tweetsFile))) {
 			String line;
 			List<Marking> markings;
 			while ((line = bReader.readLine()) != null) {
-				String[] tweet = line.split("\t");
-				if (tweet.length < 2) {
-					continue;
-				}
-				String id = tweet[0];
-				String text = tweet[1];
+				JSONObject json = new JSONObject(line);
+				
+				String id = json.getString("id_str");
+				String text = json.getString("text");
 				markings = findMarkings(getMarkingLines(annotations, id), text);
 				documents.add(new DocumentImpl(text, documentUriPrefix + id,
 						markings));
@@ -99,24 +79,15 @@ public class Microposts2016Dataset extends AbstractDataset implements
 
 		return documents;
 	}
-
+	
 	protected static List<Marking> findMarkings(Set<String> lines, String text) {
 		List<Marking> markings = new ArrayList<Marking>();
 
 		for (String line : lines) {
 			String[] annotation = line.split("\t");
 
-			int start = Integer.parseInt(annotation[1]);
-			int end = Integer.parseInt(annotation[2]);
-			int length = end - start;
-			String uri = annotation[3];
-			if (uri.startsWith("NIL")) {
-				uri = "";
-			}
-			Set<String> types = new HashSet<String>();
-			types.add(getTypeURI(annotation[5]));
-
-			markings.add(new TypedNamedEntity(start, length, uri, types));
+			String uri = WikipediaHelper.getWikipediaUri(WIKIPEDIA_DOMAIN , annotation[2]);
+			markings.add(new Annotation(uri));
 
 		}
 
@@ -147,22 +118,5 @@ public class Microposts2016Dataset extends AbstractDataset implements
 		}
 		return lines;
 	}
-
-	private static String getTypeURI(String type) {
-		switch (type.toLowerCase()) {
-		case "thing":
-			return "http://dbpedia.org/ontology/Thing";
-		case "person":
-			return "http://dbpedia.org/ontology/Person";
-		case "organization":
-			return "http://dbpedia.org/ontology/Organisation";
-		case "location":
-			return "http://dbpedia.org/ontology/Place";
-		case "event":
-			return "http://dbpedia.org/ontology/Event";
-		case "product":
-			return "http://dbpedia.org/ontology/Product";
-		}
-		return "";
-	}
+	
 }
