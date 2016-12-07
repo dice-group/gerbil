e634dd86fb7613476d5eb58f915578806a62cb08
Danilo <Danilo@ORGDMA60>
2016-12-07 22:04:20 +0100

    Weitere Implementierung am Datensatz ERD2014.

--- src/main/java/org/aksw/gerbil/dataset/impl/erd/erd_Dataset.java
+++ src/main/java/org/aksw/gerbil/dataset/impl/erd/erd_Dataset.java
@@ -21,8 +21,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 
 import java.nio.charset.Charset;
+import java.nio.file.Paths;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,11 +37,15 @@ import org.aksw.gerbil.datatypes.ErrorTypes;
 import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.Marking;
+import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
+import org.aksw.gerbil.transfer.nif.data.NamedEntity;
 
 import org.apache.commons.io.IOUtils;
 
 public class erd_Dataset extends AbstractDataset implements InitializableDataset {
 
+    private static final String FREEBASE_URI = "https://www.googleapis.com/freebase";
+    
     private String file_text;
     private String file_annotation;
     private List<Document> documents;
@@ -64,37 +70,90 @@ public class erd_Dataset extends AbstractDataset implements InitializableDataset
         this.documents = loadDocuments(new File(file_text), new File(file_annotation));
     }
     
+    private String generateDocumentUri(String fileName) {
+        
+        StringBuilder builder = new StringBuilder();
+        builder.append("http://");
+        builder.append(name);
+        builder.append('/');
+        builder.append(Paths.get(fileName).getFileName().toString());
+        
+        return builder.toString();
+        
+    }
+    
     protected List<Document> loadDocuments(File textfile, File annotationfile) throws GerbilException {
 
         if (!textfile.exists()) {
-            throw new GerbilException("The given text file (" + textfile.getAbsolutePath() + ") does not exist.",
-                    ErrorTypes.DATASET_LOADING_ERROR);
+            throw new GerbilException("The given text file (" + textfile.getAbsolutePath() + ") does not exist.", ErrorTypes.DATASET_LOADING_ERROR);
         }
         if (!annotationfile.exists()) {
-            throw new GerbilException("The given annotation file (" + annotationfile.getAbsolutePath() + ") does not exist.",
-                    ErrorTypes.DATASET_LOADING_ERROR);
+            throw new GerbilException("The given annotation file (" + annotationfile.getAbsolutePath() + ") does not exist.", ErrorTypes.DATASET_LOADING_ERROR);
         }
+        
         List<Document> docs = new ArrayList<>();
-        Map<String, String> textAnnotationsMap = new HashMap<>();
-        List<Marking> markings = new ArrayList<>();
-        BufferedReader reader = null;
+        String documentUri = generateDocumentUri(textfile.getAbsolutePath());
+        
+        Map<String, erd_Trec> textMap = new HashMap<>();
+        String text_data = "";
+        byte[] filedata = new byte[(int) textfile.length()];
+        erd_Trec datatrec = null;
+        RandomAccessFile raf;
+        
         try {
-                reader = new BufferedReader(new InputStreamReader(
-                                new FileInputStream(textfile), Charset.forName("UTF-8")));
-
-                String line = reader.readLine();
-                while (line != null) {
-                    if(!line.trim().isEmpty()){
-                        //
-                    }
-                    line = reader.readLine();
-                }
+            raf = new RandomAccessFile(textfile, "r");
+            raf.seek(0);
+            raf.readFully(filedata);
+            text_data = new String(filedata);
         } catch (IOException e) {
-                throw new GerbilException("Exception while reading dataset.", e, ErrorTypes.DATASET_LOADING_ERROR);
+            throw new GerbilException("Exception while reading text file of dataset.", e, ErrorTypes.DATASET_LOADING_ERROR);
+        }
+
+        int error = 0;
+        String[] text_split = text_data.split("\n");
+        for (String line : text_split) {
+            String[] line_part = line.split("\t");
+            String key;
+            
+            if (line_part.length != 2) {
+                error++;
+                key = "ERROR " + error;
+            } else {
+                key = line_part[0];
+            }
+            
+            datatrec = new erd_Trec(line, datatrec);
+            textMap.put(key, datatrec);
+        }
+        
+        BufferedReader reader = null;
+        List<Marking> markings = new ArrayList<>();
+        String line;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(annotationfile), Charset.forName("UTF-8")));
+
+            while ((line = reader.readLine()) != null) {
+
+                String[] line_split = line.split("\t");
+                if (line_split.length != 5) continue;
+
+                datatrec = textMap.get(line_split[0]);
+                if (datatrec != null) {
+                    int position = datatrec.getTextPosition(line_split[3]);
+                    int length = line_split[3].length();
+                    markings.add(new NamedEntity(position, length, FREEBASE_URI + line_split[2]));
+                }
+            }
+            
+        } catch (IOException e) {
+                throw new GerbilException("Exception while reading annotation file of dataset.", e, ErrorTypes.DATASET_LOADING_ERROR);
         } finally {
                 IOUtils.closeQuietly(reader);
         }
         
+        docs.add(new DocumentImpl(text_data, documentUri, markings));
+        
         return docs;
     }
+    
 }
