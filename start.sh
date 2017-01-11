9ad1cd75d78b57b645ea75a24d2fd2f60e56abe3
mroeder <roeder@informatik.uni-leipzig.de>
2017-01-11 14:32:11 +0100

    Migrated to Tomcat7 MVN plugin.

--- start.sh
+++ start.sh
@@ -33,4 +33,6 @@ if [ ! -f "$file" ]; then
 	echo "##############################################################################"  >> $file
 fi
 
-mvn clean tomcat:run -Dmaven.tomcat.port=1234
+echo "Building and starting GERBIL QA..."
+#mvn clean tomcat:run -Dmaven.tomcat.port=1234
+mvn clean org.apache.tomcat.maven:tomcat7-maven-plugin:2.2:run -Dmaven.tomcat.port=1234
