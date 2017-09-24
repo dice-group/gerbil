a1791ceb47def89de68dfafb14309dc8616c36ca
Jonathan Huthmann <j_huthmann@gmx.de>
2017-09-24 19:41:41 +0200

    fix:ExtendedQaldWebservice ; POST data now in payload instead of url

--- src/main/java/org/aksw/gerbil/annotator/impl/qa/ExtendedQALDBasedWebService.java
+++ src/main/java/org/aksw/gerbil/annotator/impl/qa/ExtendedQALDBasedWebService.java
@@ -1,10 +1,8 @@
 package org.aksw.gerbil.annotator.impl.qa;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.aksw.gerbil.annotator.QASystem;
@@ -17,115 +15,101 @@ import org.aksw.gerbil.transfer.nif.Marking;
 import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
 import org.aksw.qa.commons.datastructure.IQuestion;
 import org.aksw.qa.commons.load.json.EJQuestionFactory;
-import org.aksw.qa.commons.load.json.ExtendedJson;
 import org.aksw.qa.commons.load.json.ExtendedQALDJSONLoader;
-import org.aksw.qa.commons.load.json.QaldJson;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
-import org.apache.http.ParseException;
+import org.apache.http.Consts;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExtendedQALDBasedWebService extends AbstractHttpBasedAnnotator implements QASystem{
+public class ExtendedQALDBasedWebService extends AbstractHttpBasedAnnotator implements QASystem {
 
 	private String name;
-	private String url;
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExtendedQALDBasedWebService.class);
+	private final String url;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExtendedQALDBasedWebService.class);
 
-	
-    public static void main(String[] argc) throws GerbilException, IOException{
-    	ExtendedQALDBasedWebService service = new ExtendedQALDBasedWebService("http://wdaqua-qanary.univ-st-etienne.fr/gerbil");
-    	Document document = new DocumentImpl();
-    	document.setText("When was Barack Obama born?");
-    	service.answerQuestion(document, "en");
-    	service.close();
-    }
-    
-    public ExtendedQALDBasedWebService(String url) {
-        super();
-        this.url = url;
-    }
+	public static void main(final String[] argc) throws GerbilException, IOException {
+		ExtendedQALDBasedWebService service = new ExtendedQALDBasedWebService("https://wdaqua-qanary.univ-st-etienne.fr/gerbil-execute/wdaqua-core0,%20QueryExecuter/");
+		Document document = new DocumentImpl();
+		document.setText("When was Barack Obama born?");
+		service.answerQuestion(document, "en");
+		service.close();
+	}
 
-    public ExtendedQALDBasedWebService(String url, String name) {
-        super(name);
-        this.url = url;
-    }
-	
+	public ExtendedQALDBasedWebService(final String url) {
+		super();
+		this.url = url;
+	}
+
+	public ExtendedQALDBasedWebService(final String url, final String name) {
+		super(name);
+		this.url = url;
+	}
+
 	@Override
 	public String getName() {
 		return name;
 	}
 
 	@Override
-	public void setName(String name) {
+	public void setName(final String name) {
 		this.name = name;
 	}
 
 	@Override
-	public List<Marking> answerQuestion(Document document, String questionLang)
-			throws GerbilException {
-		
-		HttpEntity entity = new StringEntity("query="+document.getText()+"&lang="+questionLang, "UTF-8");
-		
-        HttpPost request = null;
-        try {
-            request = createPostRequest(url);
-        } catch (IllegalArgumentException e) {
-            throw new GerbilException("Couldn't create HTTP request.", e, ErrorTypes.UNEXPECTED_EXCEPTION);
-        }
-        request.setEntity(entity);
-        request.addHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED + ";charset=UTF-8");
-        request.addHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.toString());
-        request.addHeader(HttpHeaders.ACCEPT_CHARSET, "UTF-8");
+	public List<Marking> answerQuestion(final Document document, final String questionLang) throws GerbilException {
 
-        entity = null;
-        CloseableHttpResponse response = null;
-        List<Marking> ret = null;
+		RequestConfig requestConfig = RequestConfig.custom().build();
+		CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+		HttpPost httppost = new HttpPost(url);
+
+		List<NameValuePair> params = new ArrayList<>();
+		params.add(new BasicNameValuePair("query", document.getText()));
+
+		params.add(new BasicNameValuePair("lang", questionLang));
+
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, Consts.UTF_8);
+		httppost.setEntity(entity);
+
+		List<Marking> ret = null;
+		CloseableHttpResponse response = null;
+
 		try {
-            response = sendRequest(request);
-            // receive NIF document
-            entity = response.getEntity();
-            // read response and parse NIF
-            
-            try {
-        	String content = EntityUtils.toString(entity);
-        	InputStream stream = new ByteArrayInputStream(content.getBytes("UTF-8"));
-        	Object json = ExtendedQALDJSONLoader.readJson(stream, ExtendedJson.class); 
-        	List<IQuestion>  questions;
-        	if(json==null){
-        	    stream = new ByteArrayInputStream(content.getBytes("UTF-8"));
-        	    QaldJson qaldJson = (QaldJson) ExtendedQALDJSONLoader.readJson(stream, QaldJson.class);
-        	    questions = EJQuestionFactory.getQuestionsFromQaldJson(qaldJson);
-        	}
-        	else{
-        	    ExtendedJson exJson = (ExtendedJson) json;
-        	    questions = EJQuestionFactory.getQuestionsFromExtendedJson(exJson);
 
-        	}
-        	
-            	Document resultDoc = QAUtils.translateQuestion(questions.get(0), null, questionLang);
-                ret = resultDoc.getMarkings();
-            } catch (Exception e) {
-                LOGGER.error("Couldn't parse the response.", e);
-                throw new GerbilException("Couldn't parse the response.", e, ErrorTypes.UNEXPECTED_EXCEPTION);
-            }
-        } finally {
-            closeRequest(request);
-            if (entity != null) {
-                try {
-                    EntityUtils.consume(entity);
-                } catch (IOException e1) {
-                }
-            }
-            IOUtils.closeQuietly(response);
-        }
-		
+			response = client.execute(httppost);
+			// receive NIF document
+			InputStream is = response.getEntity().getContent();
+			// read response and parse NIF
+			Object json = ExtendedQALDJSONLoader.readJson(is);
+
+			List<IQuestion> questions = EJQuestionFactory.getQuestionsFromJson(json);
+
+			Document resultDoc = QAUtils.translateQuestion(questions.get(0), null, questionLang);
+			ret = resultDoc.getMarkings();
+		} catch (Exception e) {
+			LOGGER.error("Couldn't parse the response.", e);
+			throw new GerbilException("Couldn't parse the response.", e, ErrorTypes.UNEXPECTED_EXCEPTION);
+
+		} finally {
+
+			if (entity != null) {
+				try {
+					EntityUtils.consume(entity);
+				} catch (IOException e1) {
+				}
+			}
+			IOUtils.closeQuietly(response);
+			IOUtils.closeQuietly(client);
+		}
+
 		return ret;
 	}
 
