6cd17d2d56b8bfaad5ab32396604de875f0deedb
LukasBluebaum <lukasbluebaumb94@gmail.com>
2020-09-11 12:43:24 +0200

    Added RT2KB experiment to the GENIA dataset

--- src/main/java/org/aksw/gerbil/dataset/impl/genia/GENIADataset.java
+++ src/main/java/org/aksw/gerbil/dataset/impl/genia/GENIADataset.java
@@ -19,10 +19,14 @@ package org.aksw.gerbil.dataset.impl.genia;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
@@ -32,6 +36,8 @@ import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
+import com.google.common.collect.ImmutableMap;
+
 import org.aksw.gerbil.dataset.InitializableDataset;
 import org.aksw.gerbil.dataset.impl.AbstractDataset;
 import org.aksw.gerbil.datatypes.ErrorTypes;
@@ -39,7 +45,7 @@ import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.Marking;
 import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
-import org.aksw.gerbil.transfer.nif.data.NamedEntity;
+import org.aksw.gerbil.transfer.nif.data.TypedNamedEntity;
 
 public class GENIADataset extends AbstractDataset implements InitializableDataset {
 
@@ -48,11 +54,31 @@ public class GENIADataset extends AbstractDataset implements InitializableDatase
     private static final String MARKING_TAG = "cons";
     private static final String MARKING_TYPE_TAG = "sem";
 
+    private static final Map<String, String> TYPE_MAP = ImmutableMap.<String, String>builder()
+            .put("G#DNA", "http://id.nlm.nih.gov/mesh/D004247")
+            .put("G#RNA", "http://id.nlm.nih.gov/mesh/D012313")
+            .put("G#protein", "http://id.nlm.nih.gov/mesh/D011506")
+            .put("G#cell_line", "http://id.nlm.nih.gov/mesh/D002478")
+            .put("G#cell_type", "http://id.nlm.nih.gov/mesh/D002477")
+            .build();
+
     private String file;
     private List<Document> documents;
+    private int firstDocId;
+    private int lastDocId;
 
     public GENIADataset(String file) {
+        this(file, -1, -1);
+    }
+
+    public GENIADataset(String file, String firstDocId, String lastDocId) {
+        this(file, Integer.parseInt(firstDocId), Integer.parseInt(lastDocId));
+    }
+
+    public GENIADataset(String file, int firstDocId, int lastDocId) {
         this.file = file;
+        this.firstDocId = firstDocId;
+        this.lastDocId = lastDocId;
     }
 
     @Override
@@ -68,6 +94,19 @@ public class GENIADataset extends AbstractDataset implements InitializableDatase
     @Override
     public void init() throws GerbilException {
         this.documents = loadDocuments(new File(file));
+        if ((firstDocId > 0) && (lastDocId > 0)) {
+            this.documents = this.documents.subList(firstDocId - 1, lastDocId);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, GerbilException {
+        GENIADataset d = new GENIADataset("gerbil_data/datasets/genia/GENIAcorpus3.02.merged_test.xml");
+        d.init();
+        for (Document a : d.getInstances()) {
+            System.out.println(a);
+        }
+        System.out.println(d.getInstances().size());
+        d.close();
     }
 
     private List<Document> loadDocuments(File filePath) throws GerbilException {
@@ -75,34 +114,28 @@ public class GENIADataset extends AbstractDataset implements InitializableDatase
         String documentUriPrefix = "http://" + getName() + "/";
         StringBuilder currentText = new StringBuilder();
         List<Marking> currentMarkings = new ArrayList<>();
-        Deque<Integer> markingStack = new ArrayDeque<Integer>();
+        Deque<TypedNamedEntity> markingStack = new ArrayDeque<TypedNamedEntity>();
         int index = 0;
 
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         try {
             XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream(filePath));
-            while(reader.hasNext()) {
+            while (reader.hasNext()) {
                 XMLEvent nextEvent = reader.nextEvent();
 
-                //handle formatting error in dataset file, token "HMG-I(Y)" not marked as word in one sentence 
-                if(nextEvent.isCharacters() && nextEvent.asCharacters().getData().equals("HMG-I(Y)")) {
+                // handle formatting error in dataset file, token "HMG-I(Y)" not marked as word in one sentence
+                if (nextEvent.isCharacters() && nextEvent.asCharacters().getData().equals("HMG-I(Y)")) {
                     currentText.append("HMG-I(Y) ");
-                }
-
-                if(nextEvent.isStartElement()) {
+                } else if (nextEvent.isStartElement()) {
                     StartElement start = nextEvent.asStartElement();
                     String tag = start.getName().getLocalPart();
-                    if(tag.equals(WORD_TAG)) {
+                    if (tag.equals(WORD_TAG)) {
                         nextEvent = reader.nextEvent();
                         currentText.append(nextEvent.asCharacters().getData() + " ");
                     } else if (tag.equals(MARKING_TAG)) {
-                        Attribute type = start.getAttributeByName(new QName(MARKING_TYPE_TAG));
-                        //skip higher level annotations that don't appear on the surface of the text
-                        markingStack.push((type != null) ? currentText.length() : -1);
+                        markingStack.push(parseStartMarking(start, currentText));
                     }
-                }
-
-                if(nextEvent.isEndElement()) {
+                } else if(nextEvent.isEndElement()) {
                     String tag = nextEvent.asEndElement().getName().getLocalPart();
                     if(tag.equals(SENTENCE_TAG)) {
                         Document curDoc = new DocumentImpl(currentText.toString(), documentUriPrefix + index, currentMarkings);
@@ -111,11 +144,11 @@ public class GENIADataset extends AbstractDataset implements InitializableDatase
                         ++index;
                         currentMarkings = new ArrayList<>();
                     } else if (tag.equals(MARKING_TAG))  {
-                        int start = markingStack.pop();
+                        TypedNamedEntity entity = markingStack.pop();
                         //skip higher level annotations that don't appear on the surface of the text
-                        if(start != -1) {
-                            NamedEntity marking = new NamedEntity(start, currentText.length()-1-start, "");
-                            currentMarkings.add(marking);
+                        if(entity.getStartPosition() != -1) {
+                            entity.setLength(currentText.length()-1-entity.getStartPosition());
+                            currentMarkings.add(entity);
                         }
                     }
                 }
@@ -124,5 +157,20 @@ public class GENIADataset extends AbstractDataset implements InitializableDatase
             throw new GerbilException("Exception while reading dataset.", e, ErrorTypes.DATASET_LOADING_ERROR);
         }
         return docs;
+    }
+
+    private TypedNamedEntity parseStartMarking(StartElement start, StringBuilder currentText) {
+        TypedNamedEntity entity = new TypedNamedEntity(-1, -1, "", new HashSet<String>());
+        Attribute type = start.getAttributeByName(new QName(MARKING_TYPE_TAG));
+        // skip higher level annotations that don't appear on the surface of the text
+        if (type != null) {
+            String typeString = type.getValue();
+            Optional<String> key = TYPE_MAP.keySet().stream().filter(typeString::contains).findFirst();
+            if(key.isPresent()) {
+                entity.setStartPosition(currentText.length());
+                entity.getTypes().add(TYPE_MAP.get(key.get()));
+            } 
+        }
+        return entity;
     }
 }
--- src/test/java/org/aksw/gerbil/dataset/impl/genia/GENIADatasetTest.java
+++ src/test/java/org/aksw/gerbil/dataset/impl/genia/GENIADatasetTest.java
@@ -17,17 +17,24 @@
 package org.aksw.gerbil.dataset.impl.genia;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.aksw.gerbil.exceptions.GerbilException;
 import org.aksw.gerbil.transfer.nif.Document;
 import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
-import org.aksw.gerbil.transfer.nif.data.NamedEntity;
+import org.aksw.gerbil.transfer.nif.data.TypedNamedEntity;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class GENIADatasetTest {
+
+	private static final String DNA = "http://id.nlm.nih.gov/mesh/D004247";
+	private static final String RNA = "http://id.nlm.nih.gov/mesh/D012313";
+	private static final String PROTEIN = "http://id.nlm.nih.gov/mesh/D011506";
+	private static final String CELL_LINE = "http://id.nlm.nih.gov/mesh/D002478";
+	private static final String CELL_TYPE = "http://id.nlm.nih.gov/mesh/D002477";
 
 	private static final String DATASET_FILE = "src/test/resources/datasets/genia/GENIAcorpus3.02.merged_test.xml";
 	private static final String DATASET_NAME = "testDataset";
@@ -35,43 +42,39 @@ public class GENIADatasetTest {
 			new DocumentImpl("IL-2 gene expression and NF-kappa B activation through CD28 requires reactive oxygen production by 5-lipoxygenase . ",
 					"http://" + DATASET_NAME + "/0",
 					Arrays.asList(
-							new NamedEntity(0, 9, ""),
-							new NamedEntity(0, 20, ""),
-							new NamedEntity(25, 10, ""),
-							new NamedEntity(25, 21, ""),
-							new NamedEntity(55, 4, ""),
-							new NamedEntity(99, 14, ""))),
+							new TypedNamedEntity(0, 9, "", new HashSet<String>(Arrays.asList(DNA))),
+							new TypedNamedEntity(25, 10, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(55, 4, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(99, 14, "", new HashSet<String>(Arrays.asList(PROTEIN))))),
 			new DocumentImpl("Antigen complexed with major histocompatibility complex class I or II molecules on the surface of antigen presenting cells interacts with the T cell receptor ( TCR ) on the surface of T cells and initiates an activation cascade . ",
 					"http://" + DATASET_NAME + "/1",
 					Arrays.asList(
-							new NamedEntity(23, 56, ""),
-							new NamedEntity(98, 24, ""),
-							new NamedEntity(142, 15, ""),
-							new NamedEntity(160, 3, ""),
-							new NamedEntity(184, 7, ""))),
+							new TypedNamedEntity(23, 56, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(98, 24, "", new HashSet<String>(Arrays.asList(CELL_TYPE))),
+							new TypedNamedEntity(142, 15, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(160, 3, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(184, 7, "", new HashSet<String>(Arrays.asList(CELL_TYPE))))),
 			new DocumentImpl("These results imply that membrane potential changes secondary to the ligand-dependent opening of Ca(2+)-activated K+ channels are not involved in B and T lymphocyte activation and mitogenesis . ",
 					"http://" + DATASET_NAME + "/2",
 					Arrays.asList(
-							new NamedEntity(25, 18, ""),
-							new NamedEntity(25, 26, ""),
-							new NamedEntity(97, 28, ""),
-							new NamedEntity(146, 18, ""),
-							new NamedEntity(146, 45, ""))),
+							new TypedNamedEntity(97, 28, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(146, 18, "", new HashSet<String>(Arrays.asList(CELL_TYPE))))),
 			new DocumentImpl("Interestingly , GATA-2 and GATA-3 proteins also localized to the same nuclear bodies in cell lines co-expressing GATA -1 and -2 or GATA -1 and -3 gene products . ",
 					"http://" + DATASET_NAME + "/3",
 					Arrays.asList(
-							new NamedEntity(16, 6, ""),
-							new NamedEntity(27, 6, ""),
-							new NamedEntity(27, 15, ""),
-							new NamedEntity(70, 14, ""),
-							new NamedEntity(113, 14, ""),
-							new NamedEntity(131, 14, ""),
-							new NamedEntity(113, 46, ""))),
-			new DocumentImpl("Nuclear C/EBP beta was also detected in rheumatoid synovial fluid monocytes /macrophages , but not in lymphocytes or neutrophils . ",
+							new TypedNamedEntity(16, 6, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(27, 6, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(27, 15, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(113, 14, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(131, 14, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(113, 46, "", new HashSet<String>(Arrays.asList(PROTEIN))))),
+			new DocumentImpl("An additional significant finding was than TNF mRNA induced in primed cells was much more stable than in unprimed cells ( T1/2 increased 6-8-fold ) . ",
 					"http://" + DATASET_NAME + "/4",
 					Arrays.asList(
-							new NamedEntity(8, 10, ""),
-							new NamedEntity(40, 88, ""))));
+							new TypedNamedEntity(43, 3, "", new HashSet<String>(Arrays.asList(PROTEIN))),
+							new TypedNamedEntity(43, 8, "", new HashSet<String>(Arrays.asList(RNA))),
+							new TypedNamedEntity(63, 12, "", new HashSet<String>(Arrays.asList(CELL_LINE))),
+							new TypedNamedEntity(105, 14, "", new HashSet<String>(Arrays.asList(CELL_LINE))))));
 
 	@Test
 	public void test() throws GerbilException {
