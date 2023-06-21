093573eebdbf6fa165852cf44d9ce23fab39026b
Michael Röder <michael.roeder@uni-paderborn.de>
2023-06-21 21:12:46 +0200

    Updated the IndQNER dataset class. Updated the test. Added the definition of all three parts of the dataset.

--- src/main/java/org/aksw/gerbil/dataset/impl/indq/IndQNERDataset.java
+++ src/main/java/org/aksw/gerbil/dataset/impl/indq/IndQNERDataset.java
@@ -1,123 +1,38 @@
 package org.aksw.gerbil.dataset.impl.indq;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.aksw.gerbil.dataset.InitializableDataset;
-import org.aksw.gerbil.dataset.impl.AbstractDataset;
-import org.aksw.gerbil.datatypes.ErrorTypes;
-import org.aksw.gerbil.exceptions.GerbilException;
-import org.aksw.gerbil.transfer.nif.Document;
-import org.aksw.gerbil.transfer.nif.Marking;
-import org.aksw.gerbil.transfer.nif.data.DocumentImpl;
-import org.aksw.gerbil.transfer.nif.data.NamedEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.aksw.gerbil.dataset.impl.conll.CoNLLTypeRetriever;
+import org.aksw.gerbil.dataset.impl.conll.GenericCoNLLDataset;
 
 /**
- * Implementation of the IndQNERDataset class, which represents an InitializableDataset for the IndQNER dataset.
+ * Implementation of the IndQNERDataset class, which represents an
+ * InitializableDataset for the IndQNER dataset.
+ * 
+ * @author Neha
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
  */
-public class IndQNERDataset extends AbstractDataset implements InitializableDataset {
+public class IndQNERDataset extends GenericCoNLLDataset {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(IndQNERDataset.class);
+    private static final int ANNOTATION_COLUMN = 1;
+    private static final int URI_COLUMN = -1;
+    private static final CoNLLTypeRetriever TYPE_TAGS = new CoNLLTypeRetriever("GeographicalLocation", null, null, null,
+            null, "Person", null, null, null, null);
 
-    private String file;
-    private List<Document> documents;
-
-    /**
-     * Constructs a new IndQNERDataset with the specified dataset file.
-     * 
-     * @param file the path to the IndQNER dataset file
-     */
     public IndQNERDataset(String file) {
-        this.file = file;
-    }
-
-    @Override
-    public int size() {
-        return documents.size();
-    }
-
-    @Override
-    public List<Document> getInstances() {
-        return documents;
-    }
-
-    @Override
-    public void init() throws GerbilException {
-        this.documents = loadDocuments(new File(file));
-    }
-
-    /**
-     * Loads the documents from the IndQNER dataset file.
-     * 
-     * @param file the IndQNER dataset file
-     * @return a list of documents parsed from the dataset file
-     * @throws GerbilException if there is an error while loading the dataset
-     */
-    private List<Document> loadDocuments(File file) throws GerbilException {
-        List<Document> docs = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            List<String> documentTokens = new ArrayList<>();
-            List<Marking> markings = new ArrayList<>();
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                if (line.isEmpty()) {
-                    if (!documentTokens.isEmpty()) {
-                        String documentText = String.join(" ", documentTokens);
-                        Document document = new DocumentImpl(documentText);
-                        document.setMarkings(markings);
-                        docs.add(document);
-                    }
-                    documentTokens.clear();
-                    markings.clear();
-                } else {
-                    String[] parts = line.split("\t");
-                    if (parts.length == 2) {
-                        String token = parts[0];
-                        String label = parts[1];
-
-                        documentTokens.add(token);
-                        processLabel(markings, token, label);
-                    } else {
-                        LOGGER.warn("Invalid line format in IndQNER dataset file: {}", line);
-                    }
-                }
-            }
-            // Add the last document if there are remaining tokens
-            if (!documentTokens.isEmpty()) {
-                String documentText = String.join(" ", documentTokens);
-                Document document = new DocumentImpl(documentText);
-                document.setMarkings(markings);
-                docs.add(document);
-            }
-        } catch (IOException e) {
-            throw new GerbilException("Error while reading the IndQNER dataset file.", e,
-                    ErrorTypes.DATASET_LOADING_ERROR);
-        }
-        return docs;
-    }
-
-    /**
-     * Processes the label and creates a NamedEntity marking if the label indicates the
-     * beginning of a named entity.
-     * 
-     * @param markings     the list of markings for the current document
-     * @param token        the current token
-     * @param label        the label associated with the token
-     */
-    private void processLabel(List<Marking> markings, String token, String label) {
-        if (label.startsWith("B-")) {
-            String entityLabel = label.substring(2); // Remove the "B-" prefix to get the entity label
-            int tokenLength = token.length();
-            NamedEntity namedEntity = new NamedEntity(0, tokenLength, entityLabel); // Create a new NamedEntity marking
-            markings.add(namedEntity);
-        }
-        // If the label does not indicate the beginning of a named entity, it is skipped
+        super(file, ANNOTATION_COLUMN, URI_COLUMN, TYPE_TAGS);
+        TYPE_TAGS.addTypeURI("AfterlifeLocation", "https://corpus.quran.com/concept.jsp?id=afterlife-location");
+        TYPE_TAGS.addTypeURI("Allah", "https://corpus.quran.com/concept.jsp?id=allah");
+        TYPE_TAGS.addTypeURI("Angel", "https://github.com/dice-group/IndQNER/Angel"); // TODO: replace
+        TYPE_TAGS.addTypeURI("Artifact", "https://corpus.quran.com/concept.jsp?id=artifact");
+        TYPE_TAGS.addTypeURI("AstronomicalBody", "https://corpus.quran.com/concept.jsp?id=astronomical-body");
+        TYPE_TAGS.addTypeURI("Color", "https://corpus.quran.com/concept.jsp?id=color");
+        TYPE_TAGS.addTypeURI("Event", "https://corpus.quran.com/concept.jsp?id=event");
+        TYPE_TAGS.addTypeURI("Food", "https://github.com/dice-group/IndQNER/Food"); // TODO: replace
+        TYPE_TAGS.addTypeURI("HolyBook", "https://corpus.quran.com/concept.jsp?id=holy-book");
+        TYPE_TAGS.addTypeURI("Language", "https://corpus.quran.com/concept.jsp?id=language");
+        TYPE_TAGS.addTypeURI("Messenger", "https://github.com/dice-group/IndQNER/Messenger"); // TODO: replace
+        TYPE_TAGS.addTypeURI("Prophet", "https://github.com/dice-group/IndQNER/Prophet"); // TODO: replace
+        TYPE_TAGS.addTypeURI("Religion", "https://corpus.quran.com/concept.jsp?id=religion");
+        TYPE_TAGS.addTypeURI("Throne", "https://corpus.quran.com/concept.jsp?id=allah%27s-throne");
     }
 }
--- src/test/java/org/aksw/gerbil/dataset/impl/indq/IndQNERDatasetTest.java
+++ src/test/java/org/aksw/gerbil/dataset/impl/indq/IndQNERDatasetTest.java
@@ -1,88 +1,43 @@
-import org.aksw.gerbil.dataset.impl.indq.IndQNERDataset;
-import org.aksw.gerbil.exceptions.GerbilException;
-import org.aksw.gerbil.transfer.nif.Document;
-import org.aksw.gerbil.transfer.nif.Marking;
-import org.aksw.gerbil.transfer.nif.data.NamedEntity;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+package org.aksw.gerbil.dataset.impl.indq;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.aksw.gerbil.dataset.InitializableDataset;
+import org.aksw.gerbil.dataset.impl.conll.AbstractGenericCoNLLDatasetTest;
+import org.aksw.gerbil.transfer.nif.Marking;
+import org.aksw.gerbil.transfer.nif.data.TypedSpanImpl;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 @RunWith(Parameterized.class)
-public class IndQNerDatasetTest {
+public class IndQNERDatasetTest extends AbstractGenericCoNLLDatasetTest {
+
+    public IndQNERDatasetTest(String fileContent, String text, Marking expectedMarking, int documentId, int markingId) {
+        super(fileContent, text, expectedMarking, documentId, markingId);
+    }
+
+    @Override
+    public InitializableDataset createDataset(File file) {
+        return new IndQNERDataset(file.getAbsolutePath());
+    }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         List<Object[]> testConfigs = new ArrayList<>();
-        //test configurations
-        testConfigs.add(new Object[]{
-                "Tidak adab sesuatu yang lebih kecil dan yang lebih besar daripada itu, " +
-                "kecuali semua tercatat dalam kitab yang nyata (Lauh Mahfuz ).",
-                "Example tweet 1",
-                new String[]{"Example expected token 1"}
-        });
-        testConfigs.add(new Object[]{
-                "Ketahuilah bahwa sesungguhnya ( bagi ) para wali Allah-B-Allah itu " +
-                "tidak ada rasa takut yang menimpa mereka dan mereka pun tidak bersedih.",
-                "Example tweet 2",
-                new String[]{"Example expected token 2"}
-        });
+        // test configurations
+        testConfigs.add(new Object[] {
+                "Dia	O\nmenarik	O\ntangannya	O\n,	O\ntiba-tiba	O\nia	O\n(	O\ntangan	O\nitu	O\n)	O\nmenjadi	O\nputih	B-Color\n(	O\nbercahaya	O\n)	O\nbagi	O\norang-orang	O\nyang	O\nmelihat	O\n(	O\n-nya	O\n)	O\n.	O\n\nPara	O\npemuka	O\nkaum	O\nFir‘aun	B-Person\nberkata	O\n,	O\n“	O\nSesungguhnya	O\norang	O\nini	O\nbenar-benar	O\npenyihir	O\nyang	O\nsangat	O\npandai	O\n.	O\n\nDia	O\nhendak	O\nmengusir	O\nkamu	O\ndari	O\nnegerimu	O\n.	O",
+                "Dia menarik tangannya , tiba-tiba ia ( tangan itu ) menjadi putih ( bercahaya ) bagi orang-orang yang melihat ( -nya ) . ",
+                new TypedSpanImpl(60, 5, "https://corpus.quran.com/concept.jsp?id=color"), 0, 0 });
+        testConfigs.add(new Object[] {
+                "Dia	O\nmenarik	O\ntangannya	O\n,	O\ntiba-tiba	O\nia	O\n(	O\ntangan	O\nitu	O\n)	O\nmenjadi	O\nputih	B-Color\n(	O\nbercahaya	O\n)	O\nbagi	O\norang-orang	O\nyang	O\nmelihat	O\n(	O\n-nya	O\n)	O\n.	O\n\nPara	O\npemuka	O\nkaum	O\nFir‘aun	B-Person\nberkata	O\n,	O\n“	O\nSesungguhnya	O\norang	O\nini	O\nbenar-benar	O\npenyihir	O\nyang	O\nsangat	O\npandai	O\n.	O\n\nDia	O\nhendak	O\nmengusir	O\nkamu	O\ndari	O\nnegerimu	O\n.	O",
+                "Para pemuka kaum Fir‘aun berkata , “ Sesungguhnya orang ini benar-benar penyihir yang sangat pandai . ",
+                new TypedSpanImpl(17, 7, "http://dbpedia.org/ontology/Person"), 1, 0 });
 
         return testConfigs;
     }
 
-    private String text;
-    private String tweet;
-    private String[] expectedTokens;
-
-    public IndQNerDatasetTest(String text, String tweet, String[] expectedTokens) {
-        this.text = text;
-        this.tweet = tweet;
-        this.expectedTokens = expectedTokens;
-    }
-
-    @Test
-    public void testIndQNerDataset() throws GerbilException {
-        // Create an instance of the IndQNerDataset
-        IndQNERDataset indQNerDataset = new IndQNERDataset();
-
-        // Initialize the dataset
-        indQNerDataset.init();
-
-        // Get the instances/documents from the dataset
-        List<Document> documents = indQNerDataset.getInstances();
-
-        // Ensure that the documents are not null and there is at least one document
-        Assert.assertNotNull(documents);
-        Assert.assertTrue(documents.size() > 0);
-
-        // Iterate over the markings in the first document
-        Document document = documents.get(0);
-        List<Marking> markings = document.getMarkings();
-        Assert.assertNotNull(markings);
-        Assert.assertTrue(markings.size() > 0);
-
-        // Verify that the markings are NamedEntities
-        for (Marking marking : markings) {
-            Assert.assertTrue(marking instanceof NamedEntity);
-            NamedEntity namedEntity = (NamedEntity) marking;
-
-            // Get the mention from the named entity
-            String mention = tweet.substring(namedEntity.getStartPosition(), namedEntity.getStartPosition() + namedEntity.getLength());
-
-            // Perform assertions to check if the expected tokens match the extracted mentions
-            for (String expectedToken : expectedTokens) {
-                if (mention.contains(expectedToken)) {
-                    Assert.assertEquals(expectedToken, mention);
-                }
-            }
-        }
-
-        // Clean up and close the dataset
-        indQNerDataset.close();
-    }
 }
