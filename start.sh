119962e90291242531cad77f8d07997ff47b69cc
Michael Röder <michael.roeder@uni-paderborn.de>
2024-01-04 14:28:05 +0100

    Fixed the gerbil_data zip file.

--- start.sh
+++ start.sh
@@ -113,14 +113,14 @@ function yesno()
 # Check for dependencies
 echo "Checking dependencies..."
 file="gerbil_data/gerbil_data.zip"
-url="https://hobbitdata.informatik.uni-leipzig.de/gerbil/gerbil_data-1.2.9-SNAPSHOT.zip"
+url="https://files.dice-research.org/projects/GERBIL/gerbil_data-1.2.9.zip"
 
 if [ ! -d "gerbil_data" ]; then
     mkdir -p "gerbil_data" || error "Could not create gerbil_data directory"
     mkdir -p "gerbil_data/cache" || error "Could not create gerbil_data/cache directory"
     if [ ! -f "$file" ]; then
         echo "Downloading dependencies ... ($url)"
-        curl --retry 4 -L -o "$file" "$url"
+        curl -o "$file" "$url"
 
         if [ ! -f "$file" ]; then
             error "Couldn't downloading dependency data: $file"
