6303deaa822de07d782746742793904b5d22160c
Anoop RJ <arjanoop@gmail.com>
2024-06-06 20:51:13 +0200

    fixed dataset path in OKEChallengeNIFTest

--- src/test/java/org/aksw/gerbil/OKEChallengeNIFTest.java
+++ src/test/java/org/aksw/gerbil/OKEChallengeNIFTest.java
@@ -30,7 +30,7 @@ public class OKEChallengeNIFTest {
 
     public static final String FILES[] = new String[] {
             // "gerbil_data/datasets/spotlight/dbpedia-spotlight-nif.ttl"};
-            "gerbil_data/datasets/KORE50/kore50-nif.ttl",
+            "gerbil_data/datasets/KORE50/DYWC/kore50-nif-dbpedia.ttl",
             "C:/users/Micha/workspace/oke-challenge/GoldStandard_sampleData/task2/dataset_task_2.ttl",
             "C:/users/Micha/workspace/oke-challenge/GoldStandard_sampleData/task1/dataset_task_1.ttl",
             "C:/users/Micha/workspace/oke-challenge/example_data/task1.ttl",
